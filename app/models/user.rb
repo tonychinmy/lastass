0fac9a89d43f3379b07fcabf9409a6890ca4af72
Tony Chin <tonychin0101@gmail.com>
2017-07-04 15:52:36 +0800

    fb login done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
 
 	validates :username, :email, uniqueness: true
 	has_many :ideas
+	has_many :authentications, dependent: :destroy
+
+    def self.create_with_auth_and_hash(authentication, auth_hash)
+      user = self.create!(
+        username: auth_hash["username"],
+        email: auth_hash["extra"]["raw_info"]["email"],
+        password: SecureRandom.hex(3)
+      )
+      user.authentications << authentication
+      return user
+    end
+
+    # grab fb_token to access Facebook for user data
+    def fb_token
+      x = self.authentications.find_by(provider: 'facebook')
+      return x.token unless x.nil?
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as:'login'
   get 'logout', to: 'sessions#destroy', as:'logout'
 
+
   resources :users
   resources :sessions
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
   get 'welcome/index'
 
   root 'welcome#index'
