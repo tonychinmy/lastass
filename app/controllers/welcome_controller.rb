30bd8de84e62c418322358f3ffba6f9ed9d14921
Tony Chin <tonychin0101@gmail.com>
2017-07-05 22:06:31 +0800

    search look

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,4 +7,10 @@ class WelcomeController < ApplicationController
    	user_ideas = Idea.where(user_id: current_user)
    	@user_ideas = user_ideas.order("created_at DESC").page params[:page]
   end
+
+  private
+	# A list of the param names that can be used for filtering the Product list
+  def filtering_params(params)
+	  params.slice(:search, :title, :content)
+	end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,10 @@
 class Idea < ApplicationRecord
+	
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 	paginates_per 8
+	# include PgSearch
+	# pg_search_scope :search_by_title, :against => :title
+	# pg_search_scope :search_by_content, :against => :content
+	scope :title, -> (title) { where "LOWER(title) LIKE LOWER(?)", "%#{title}%" }
 end
